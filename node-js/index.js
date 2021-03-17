50d9bdde49d23893155225cf53a87f18f167f1ba
nacy <513428396@qq.com>
2021-03-17 15:55:15 +0800

    修复跨域bug

--- node-js/index.js
+++ node-js/index.js
@@ -13,9 +13,10 @@ app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Headers', 'mytoken');
+
     next();
 });
+
 app.get('/async1', (req, res) => {
     res.send('hello1')
 })
