3c360cccb1b84fc0e5b8d6fed4571ab498e3ee5c
nacy <513428396@qq.com>
2021-03-18 16:50:40 +0800

    上传作业

--- node-js/mybook/index.js
+++ node-js/mybook/index.js
@@ -4,6 +4,15 @@ const router = require('./router.js');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); //这个表示任意域名都可以访问，这样写不能携带cookie了。
+    //res.header('Access-Control-Allow-Origin', 'http://www.baidu.com'); //这样写，只有www.baidu.com 可以访问。
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS'); //设置方法
+
+    next();
+});
+
 // 启动静态资源服务
 app.use(express.static('public'));
 
