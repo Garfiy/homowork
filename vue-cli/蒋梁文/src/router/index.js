0a2ce5cdcd391f7287a07e9bf36a8e2cb80bfc3b
deeppink722 <396741739@qq.com>
2021-03-23 21:23:35 +0800

    蒋梁文

---
+++ vue-cli/蒋梁文/src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import H0323 from '../views/2021-03-23';
+
+Vue.use(VueRouter);
+
+const routes = [{
+    path: '/',
+    redirect: '../views/2021-03-23',
+}, {
+    path: '/views/2021-03-23',
+    component: H0323,
+}];
+
+const router = new VueRouter({
+    routes,
+});
+
+export default router;
