1bccbf696b82efb0da34309f65e82d8d81b685a3
kobe <2962181782@qq.com>
2021-03-24 20:12:06 +0800

    唐锦标

--- vue-cli/唐锦标/src/router/index.js
+++ vue-cli/唐锦标/src/router/index.js
@@ -1,16 +1,50 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 // import Home from "../views/Home.vue";
-import h0323 from '../views/2021-03-23'
+import h0323 from '../views/2021/0323';
+import MyMenu from '../views/myMenu.vue';
 
 Vue.use(VueRouter);
 
 const routes = [{
     path: '/',
-    redirect: '../views/2021-03-23'
+    redirect: '/homework'
 }, {
-    path: '/views/2021-03-23',
-    component: h0323,
+    path: '/homework',
+    component: MyMenu,
+    children: [{
+        //子路由的路径可以写相对路径
+        path: '2021/0323',
+        component: h0323,
+    }, {
+        //子路由的路径可以写相对路径
+        path: '2021/0324',
+        component: () =>
+            import ('../views/2021/0324/index.vue'),
+    }]
+}, {
+    path: '/study',
+    component: MyMenu,
+    children: [{
+        path: '2021/0324/:name',
+        name: '编程导航',
+        component: () =>
+            import ('../views/2021/0324/Dynamic.vue'),
+    }, {
+        path: '2021/0324/',
+        component: () =>
+            import ('../views/2021/0324/Dynamic.vue'),
+    }, {
+        path: "2021/0324class",
+        components: {
+            default: () =>
+                import ('../views/2021/0324/index.vue'),
+            header: () =>
+                import ('../views/2021/0324/index.vue'),
+            footer: () =>
+                import ('../views/2021/0324/index.vue'),
+        }
+    }]
 }];
 
 const router = new VueRouter({
