747b0de165d092db5784b34907006b7dec2e0aef
dysthn <1690968562@qq.com>
2021-03-24 20:31:39 +0800

    thn

--- vue-cli/谭欢妮/src/router/index.js
+++ vue-cli/谭欢妮/src/router/index.js
@@ -1,17 +1,36 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/2021/0323/vue.vue";
+import Home from "../views/2021/0323";
+import MyMenu from '../views/myMenu.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path:'/',
-    redirect:'../views/2021/0323/vue.vue',
-  },{
-    path:'/views/2021/0323/vue.vue',
-    component:Home,
-  },
+    // 作业
+    path: '/',
+    redirect: '/homework',
+  }, {
+    path: '/homework',
+    component: MyMenu,
+    children: [{
+      path: '2021/0323',
+      component: Home,
+    }, {
+      path: '2021/0324',
+      component: function () {
+        return import('../views/2021/0324')
+      }
+    }],
+  }, {
+    // 学习
+    path: '/study',
+    component: MyMenu,
+    children:[{
+      path:'2021/0324',
+      component:()=>import('../views/2021/0324/Dynamic.vue'),
+    }]
+  }
 ];
 
 const router = new VueRouter({
---
+++ vue-cli/谭欢妮/src/views/2021/0324/Dynamic.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <p>动态路由{{$route.params.name}}</p>
+        <p>动态路由{{$route.query.name}}</p>
+    </div>
+</template>
