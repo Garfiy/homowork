c6bcfeba3f3859f5440e3852ba3af0c27656801f
PCQ <2308135799@qq.com>
2021-03-24 19:19:18 +0800

    彭超群

---
+++ vue-cli/彭超群/src/views/2021/0324/index.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <!-- 保持template这个标签内容只有一个标签 -->
+  <div>
+    <h1 v-text="msg"></h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "2021-0324",
+    };
+  },
+};
+</script>
