12e32413bd96cd33b7cc40a9a45cefb5953393cb
kobe <2962181782@qq.com>
2021-03-25 20:50:42 +0800

    唐锦标

---
+++ vue-cli/唐锦标/dist/js/chunk-2d2371b0.f73054b6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2371b0"],{fa65:function(e,n,a){"use strict";a.r(n);var r=function(){var e=this,n=e.$createElement,a=e._self._c||n;return a("div",[a("p",[e._v("动态路由:params:"+e._s(e.$route.params.name))]),a("p",[e._v("动态路由:query:"+e._s(e.$route.query.name))])])},u=[],t=a("2877"),s={},p=Object(t["a"])(s,r,u,!1,null,null,null);n["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d2371b0.f73054b6.js.map
