eba8469690be276e02e103ebf7bf5ea5ee598d51
yun <2368999352@qq.com>
2021-03-25 17:30:32 +0800

    lxs

---
+++ vue-cli/刘香狮/src/views/study/March/No25/Homeworkyesterday.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="6">
+        <el-input v-model="value1" placeholder="请输入内容" @change="onKeyEnent"></el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-select v-model="value2" placeholder="请选择" @change="sum">
+          <el-option v-for="item in options" :key="item" :label="item" :value="item"></el-option>
+        </el-select>
+      </el-col>
+      <el-col :span="6">
+        <el-input v-model="value3" placeholder="请输入内容" ref="input2"  @change="onkeyEneter"></el-input>
+      </el-col>
+      <el-col :span="1">
+        <el-button @click="sum">=</el-button>
+      </el-col>
+      <el-col :span="7">
+        <span class="result">{{result}}</span>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      value1: "",
+      value2: "+",
+      value3: "",
+      options: ["+", "-", "*", "/"],
+      result: "",
+    };
+  },
+  methods: {
+    sum() {
+      if (this.value1 && this.value2 && this.value3) {
+        this.result = eval(this.value1 + this.value2 + this.value3);
+      }
+    },
+    onKeyEnent(){
+        this.$refs.input2.focus();
+    },
+    onkeyEneter(){
+        this.sum()
+    }
+  },
+};
+</script>
+<style lang="less">
+.result {
+  line-height: 40px;
+}
+</style>
