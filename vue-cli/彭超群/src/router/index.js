67ab5e9cf29a4cc991dca91db3a0b421e626bc44
PCQ <2308135799@qq.com>
2021-03-25 22:01:40 +0800

    彭超群

--- vue-cli/彭超群/src/router/index.js
+++ vue-cli/彭超群/src/router/index.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import H0323 from "../views/2021/0323";
 import MyMenu from "../views/myMenu.vue";
+
+
+// 解决ElementUI导航栏中的vue-router在3.0版本以上重复点菜单报错问题
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err);
+};
 Vue.use(VueRouter);
 
 const routes = [{
@@ -12,26 +19,68 @@ const routes = [{
     path: '/homework',
     component: MyMenu,
     children: [{
-        //子路由路径可以写相对路径
-        path: '2021/0323',
-        component: H0323
+        path: '2021/0325',
+        component: () =>
+            import ('../views/2021/0325/index.vue')
     }, {
         path: '2021/0324',
         //懒加载,需要显示模式,在需要显示的时候才加载
         component: () =>
             import ("../views/2021/0324/index.vue")
+    }, {
+        //子路由路径可以写相对路径
+        path: '2021/0323',
+        component: H0323
     }]
 }, {
     // 学习的内容
     path: '/study',
     component: MyMenu,
     children: [{
+        path: '2021/0325/s03',
+        //将路由上的参数传到组件的props属性中
+        props: route => {
+            return {
+                id: route.query.id
+            }
+        },
+        component: () =>
+            import ('../views/2021/0325/s03.vue')
+    }, {
+        path: '2021/0325/s03/:id',
+        //将路由上的参数传到组件的props属性中
+        props: true,
+        component: () =>
+            import ('../views/2021/0325/s03.vue')
+    }, {
+        path: '2021/0325/02',
+        alias: '/liebiao',
+        redirect: () => {
+            let flag = true;
+            if (flag) {
+                return '/homework/2021/0324';
+            } else {
+                return '2021/0324class';
+            }
+        }
+    }, {
+        path: '2021/0325/01',
+        //重新向可以传达对象值
+        redirect: {
+            // name: '编程导航',
+            //   /开头 那么就从根路由开启导航
+            path: '/homework/2021/0324',
+            params: {
+                name: '123'
+            }
+        }
+    }, {
         path: '2021/0324/:name',
+        name: '编程导航',
         component: () =>
             import ('../views/2021/0324/dynamic.vue')
     }, {
         path: '2021/0324',
-        name: '编程导航',
         component: () =>
             import ('../views/2021/0324/dynamic.vue')
     }, {
