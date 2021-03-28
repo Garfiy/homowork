be47dbe31fa640f33dfd940d2771842200111786
deeppink722 <77434793+deeppink722@users.noreply.github.com>
2021-03-28 21:47:00 +0800

    蒋梁文

--- vue-cli/蒋梁文/src/views/2021/0328/index.vue
+++ vue-cli/蒋梁文/src/views/2021/0328/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-    <el-form ref="form" :model="form" label-width="80px">
+        <el-form ref="form" :model="form" label-width="80px">
   <el-form-item label="活动名称">
     <el-input v-model="form.name"></el-input>
   </el-form-item>
@@ -16,7 +16,7 @@
     </el-col>
     <el-col class="line" :span="2">-</el-col>
     <el-col :span="11">
-      <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+      <el-time-picker type="data" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
     </el-col>
   </el-form-item>
   <el-form-item label="即时配送">
@@ -38,20 +38,15 @@
   </el-form-item>
   <el-form-item label="活动形式">
     <el-input type="textarea" v-model="form.desc"></el-input>
-    
   </el-form-item>
   <el-form-item>
     <el-button type="primary" @click="onSubmit">立即创建</el-button>
     <el-button>取消</el-button>
-    <div v-html="form.arr">content</div>
+    <div v-html="form.content"></div>
   </el-form-item>
- 
-    
 </el-form>
-</div>
+    </div>
 </template>
-
-
 <script>
   export default {
     data() {
@@ -61,22 +56,17 @@
           region: '',
           date1: '',
           date2: '',
-          delivery: false,
+          delivery:false,
           type: [],
           resource: '',
           desc: '',
-          arr:'',
+          content:''
         }
       }
     },
     methods: {
       onSubmit() {
-        if(this.form.delivery){
-          this.form.delivery='yes';
-        }else{
-           this.form.delivery='no';
-        }
-         this.form.arr='活动名称为：' + this.form.name + '<br>互动区域为：'+this.form.region + '<br>活动时间为：'+ this.form.date1 + this.form.date2 +'<br>即时配送:'+ this.form.delivery+'<br>活动性质为：'+this.form.type+'<br>特殊资源Wie：' +this.form.type+'<br>活动形式为：'+this.form.desc;
+        this.form.content='活动名称：'+this.form.name+'<br>活动区域：'+this.form.region+'<br>活动时间:'+this.form.date1+'<br>选择时间:'+this.form.date2+'<br>即时配送:'+this.form.delivery+'<br>活动性质:'+this.form.type+'<br>特殊资源:'+this.form.resource+'<br>活动形式:'+this.form.desc;
       }
     }
   }
