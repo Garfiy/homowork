17eb9363abf396df3839c03a2c283608345d61cd
cc <1463471570@qq.com>
2021-03-28 20:41:34 +0800

    提交

---
+++ vue-cli/信梦鑫/src/views/2021/0326/c1.vue
@@ -0,0 +1,42 @@
+<template>
+<transition name="el-fade-in-linear">
+    <h1>组件内部前置{{$route.meta.title}}</h1>
+</transition>
+    
+</template>
+<script>
+export default {
+    beforeRouteEnter (to, from, next) {
+        //在渲染该组件的对应路由被 confirm 前调用
+        //不能获取组件实例 `this`
+        //因为当守卫执行前,组件实例还没被创建
+        to,from;
+        //当我们需要使用this对象时
+        //可以 以回调的形式获取到this指向的vm对象
+        setTimeout(()=>{
+               next(vm=>{
+            console.log(vm);
+        });
+        },2000)
+     
+        console.log('enter',from);
+        console.log('to',to);
+    },
+    beforeRouteUpdate (to, from, next) {
+        //在当前路由改变,但是该组件被复用时调用
+        //举例来说,对于一个带有动态参数的路径 /foo/:id, 在/foo/1 和 /foo/2之间跳转的时候
+        //由于会渲染同样的 foo 组件,因此组件实例会被复用,而这个钩子就会在这个情况下被调用
+        //可以访问组件实例 `this`
+        from;
+        next();
+        console.log('update',to);
+    },
+    beforeRouteLeave (to, from, next) {
+        //导航离开之前该组件的对应路由时调用
+        //可以访问组件的实例  `this`
+        from;
+        next();
+        console.log('leave',to);
+    }
+}
+</script>
