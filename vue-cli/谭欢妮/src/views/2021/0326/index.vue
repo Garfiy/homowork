2be9802a0216c0ef7576bf3f24da331f16c15175
dysthn <1690968562@qq.com>
2021-03-29 21:00:18 +0800

    thn

--- vue-cli/谭欢妮/src/views/2021/0326/index.vue
+++ vue-cli/谭欢妮/src/views/2021/0326/index.vue
@@ -1,108 +1,157 @@
 <template>
   <div>
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="活动名称">
-        <el-input v-model="form.name"></el-input>
+    <el-form
+      :model="ruleForm"
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="活动名称" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
       </el-form-item>
-      <el-form-item label="活动区域">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="北京" value="shanghai"></el-option>
-          <el-option label="云南" value="beijing"></el-option>
+      <el-form-item label="活动区域" prop="region">
+        <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
+          <el-option value="北京">北京</el-option>
+          <el-option value="云南">云南</el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="活动时间">
+      <el-form-item label="活动时间" required>
         <el-col :span="11">
-          <el-date-picker
-            type="date"
-            placeholder="选择日期"
-            v-model="form.date"
-            style="width: 100%"
-          ></el-date-picker>
+          <el-form-item prop="date1">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="ruleForm.date1"
+              style="width: 100%"
+              value-format="yyyy / MM / dd"
+            ></el-date-picker>
+          </el-form-item>
         </el-col>
         <el-col class="line" :span="2">-</el-col>
         <el-col :span="11">
-          <el-time-picker
-            placeholder="选择时间"
-            v-model="form.test"
-            style="width: 100%"
-          ></el-time-picker>
+          <el-form-item prop="date2">
+            <el-time-picker
+              placeholder="选择时间"
+              v-model="ruleForm.date2"
+              style="width: 100%"
+              value-format="A hh: mm: ss"
+            ></el-time-picker>
+          </el-form-item>
         </el-col>
       </el-form-item>
-      <el-form-item label="即时配送">
-        <el-switch v-model="form.delivery"></el-switch>
+      <el-form-item label="即时配送" prop="delivery">
+        <el-switch
+          v-model="ruleForm.delivery"
+          active-value="是"
+          inactive-value="否"
+        ></el-switch>
       </el-form-item>
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="form.type">
+      <el-form-item label="活动性质" prop="type">
+        <el-checkbox-group v-model="ruleForm.type">
           <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
           <el-checkbox label="地推活动" name="type"></el-checkbox>
           <el-checkbox label="线下主题活动" name="type"></el-checkbox>
           <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
         </el-checkbox-group>
       </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
+      <el-form-item label="特殊资源" prop="resource">
+        <el-radio-group v-model="ruleForm.resource">
           <el-radio label="线上品牌商赞助"></el-radio>
           <el-radio label="线下场地免费"></el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="活动形式">
-        <el-input type="textarea" v-model="form.desc"></el-input>
+      <el-form-item label="活动形式" prop="desc">
+        <el-input type="textarea" v-model="ruleForm.desc"></el-input>
       </el-form-item>
-      <el-row v-show="vShow">
-        <el-col :span="24">
-          <div
-            class="grid-content bg-purple-dark"
-            style="border: 1px solid green"
-            v-html="output"
-          ></div>
-        </el-col>
-      </el-row>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button @click="outputK">取消</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >立即创建</el-button
+        >
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
       </el-form-item>
     </el-form>
+    <div v-show="isShow">
+      <p>活动名称: {{ ruleForm.name }}</p>
+      <p>活动区域: {{ ruleForm.region }}</p>
+      <p>活动时间(日期): {{ ruleForm.date1 }}</p>
+      <p>活动时间(时间): {{ ruleForm.date2 }}</p>
+      <p>即时配送: {{ ruleForm.delivery }}</p>
+      <p>活动性质: {{ ruleForm.type.join(", ") }}</p>
+      <p>特殊资源: {{ ruleForm.resource }}</p>
+      <p>活动形式: {{ ruleForm.desc }}</p>
+    </div>
   </div>
 </template>
 <script>
+import store from "../../../store/store";
+
 export default {
   data() {
     return {
-      form: {
+      isShow: false,
+      ruleForm: {
         name: "",
         region: "",
-        date: "",
-        test: "",
+        date1: "",
+        date2: "",
         delivery: false,
         type: [],
         resource: "",
         desc: "",
       },
-      output: "",
-      vShow: false,
+      rules: {
+        name: [
+          { required: true, message: "请输入活动名称", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+        region: [
+          { required: true, message: "请选择活动区域", trigger: "change" },
+        ],
+        date1: [
+          {
+            required: true,
+            message: "请选择日期",
+            trigger: "change",
+          },
+        ],
+        date2: [
+          {
+            required: true,
+            message: "请选择时间",
+            trigger: "change",
+          },
+        ],
+        type: [
+          {
+            type: "array",
+            required: true,
+            message: "请至少选择一个活动性质",
+            trigger: "change",
+          },
+        ],
+        resource: [
+          { required: true, message: "请选择活动资源", trigger: "change" },
+        ],
+        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }],
+      },
     };
   },
   methods: {
-    onSubmit() {
-      this.vShow = true;
-      var del = "";
-      if (this.form.delivery == true) {
-        del = "是";
-      } else {
-        del = "否";
-      }
-      this.output =
-        "活动名称: " +this.form.name +
-        "<br><br>活动区域: " +this.form.region +
-        "<br><br>活动时间: " +this.form.test +
-        "<br><br>即时配送: " +del +
-        "<br><br>活动性质: " + this.form.type +
-        "<br><br>特殊资源: " + this.form.resource +
-        "<br><br>活动形式: " + this.form.desc;
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.isShow = true;
+          store.form.push(this.ruleForm);
+          this.$router.push("/homework/2021/0329");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
-    outputK() {
-      this.output = "";
-      this.vShow = true;
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     },
   },
 };
