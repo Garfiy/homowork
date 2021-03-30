99634ceeede9e2e5593435bd9f1040d510cc1995
钱棋 <1828563934@qq.com>
2021-03-30 11:20:19 +0800

    钱棋

--- vue-cli/钱棋/src/views/myMenu.vue
+++ vue-cli/钱棋/src/views/myMenu.vue
@@ -1,5 +1,7 @@
 <template>
+  <!-- 一行 -->
   <el-row>
+    <!-- 一列 -->
     <el-col :span="3">
       <!-- 菜单标签 -->
       <el-menu :router="true">
@@ -7,28 +9,16 @@
         <el-submenu index="1">
           <template slot="title">homework</template>
           <el-menu-item-group>
-<<<<<<< HEAD
             <el-menu-item index="/homework/2021/0323">0323</el-menu-item>
             <el-menu-item index="/homework/2021/0324">0324</el-menu-item>
             <el-menu-item index="/homework/2021/0325">0325</el-menu-item>
             <el-menu-item index="/homework/2021/0326">0326</el-menu-item>
             <el-menu-item index="/homework/2021/0329">0329</el-menu-item>
-=======
-            <el-menu-item index="/homework/2021/0323">20210323</el-menu-item>
-            <el-menu-item index="/homework/2021/0324">20210324</el-menu-item>
-<<<<<<< HEAD
->>>>>>> b32d1135c695c90dfb40e1c55cd47ecaa286ff68
-=======
-            <el-menu-item index="/homework/2021/0325">20210325</el-menu-item>
-            <el-menu-item index="/homework/2021/0326">20210326</el-menu-item>
->>>>>>> parent of b32d113 (彭超群)
           </el-menu-item-group>
         </el-submenu>
         <!-- 再来一个一级标签 -->
         <el-submenu index="2">
           <template slot="title">study</template>
-<<<<<<< HEAD
-<<<<<<< HEAD
 
           <el-submenu index="03-29">
             <template slot="title">0329</template>
@@ -62,38 +52,12 @@
               <el-menu-item index="/study/2021/0325/02">
                 路由重定向2
               </el-menu-item>
-=======
-          <el-submenu index="03-26">
-            <template slot="title">0326</template>
-            <el-menu-item-group>
-              <el-menu-item index="/study/2021/0326/01"
-                >组件内前置守卫</el-menu-item
-              >
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="03-25">
-            <template slot="title">0325</template>
-            <el-menu-item-group>
-              <el-menu-item index="/study/2021/0325/03?id=456"
-                >路由query传参</el-menu-item
-              >
-              <el-menu-item index="/study/2021/0325/03/55"
-                >路由params传参</el-menu-item
-              >
-              <el-menu-item index="/study/2021/0325/01"
-                >路由重定向1</el-menu-item
-              >
-              <el-menu-item index="/study/2021/0325/02"
-                >路由重定向2</el-menu-item
-              >
->>>>>>> parent of b32d113 (彭超群)
               <el-menu-item index="/liebiao">路由重定向3</el-menu-item>
             </el-menu-item-group>
           </el-submenu>
           <el-submenu index="03-24">
             <template slot="title">0324</template>
             <el-menu-item-group>
-<<<<<<< HEAD
               <el-menu-item index="/study/2021/0324class">
                 命名路由
               </el-menu-item>
@@ -103,17 +67,6 @@
               <el-menu-item index="/study/2021/0324?name=123">
                 动态路由query
               </el-menu-item>
-=======
-              <el-menu-item index="/study/2021/0324class"
-                >命名路由</el-menu-item
-              >
-              <el-menu-item index="/study/2021/0324/老王"
-                >动态路由params</el-menu-item
-              >
-              <el-menu-item index="/study/2021/0324?name=123"
-                >动态路由query</el-menu-item
-              >
->>>>>>> parent of b32d113 (彭超群)
               <el-menu-item
                 index="123"
                 @click="
@@ -122,14 +75,9 @@
                     query: { name: '编程导航query' },
                   })
                 "
-<<<<<<< HEAD
               >
                 编程式导航
               </el-menu-item>
-=======
-                >编程式导航</el-menu-item
-              >
->>>>>>> parent of b32d113 (彭超群)
               <el-menu-item
                 index="1222"
                 @click="
@@ -138,30 +86,15 @@
                     params: { name: 'params编程导航' },
                   })
                 "
-<<<<<<< HEAD
               >
                 编程式导航params
               </el-menu-item>
             </el-menu-item-group>
           </el-submenu>
-=======
-          <el-menu-item-group>
-              <el-menu-item index="/study/2021/0324class">命名路由</el-menu-item>
-              <el-menu-item index="/study/2021/0324/王">动态路由parmas</el-menu-item>
-              <el-menu-item index="/study/2021/0324?name=123">动态路由query</el-menu-item>
-              <el-menu-item @click="$router.push({path:'/study/2021/0324',query:{ name : '编程导航query' }})">编程式导航</el-menu-item>
-              <el-menu-item @click="$router.push({name:'编程导航',params : { name : 'params编程导航' } })">编程式导航params</el-menu-item>
-          </el-menu-item-group>
->>>>>>> b32d1135c695c90dfb40e1c55cd47ecaa286ff68
-=======
-                >编程式导航params</el-menu-item
-              >
-            </el-menu-item-group>
-          </el-submenu>
->>>>>>> parent of b32d113 (彭超群)
         </el-submenu>
       </el-menu>
     </el-col>
+
     <!-- 放视图 -->
     <el-col :span="21">
       <router-view name="header"></router-view>
@@ -171,7 +104,8 @@
   </el-row>
 </template>
 
-<style lang="less"></style>
+<style lang="less">
+</style>
 
 <script>
 export default {};
