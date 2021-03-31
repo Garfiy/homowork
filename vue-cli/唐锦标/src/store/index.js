90a824b212f56be71b8d342ce885043463c7c18e
kobe <2962181782@qq.com>
2021-03-31 21:38:09 +0800

    唐锦标

--- vue-cli/唐锦标/src/store/index.js
+++ vue-cli/唐锦标/src/store/index.js
@@ -4,5 +4,28 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+    //状态值
+    state: {
+        count: 0,
+        name: 'zs',
+        age: "20",
+        sex: "男"
+    },
+    //修改状态
+    //mutations中的方法~第一个参数固定是state
+    mutations: {
+        //修改年龄
+        setAge(state, value) {
+            state.age = value;
+        },
+        setName(state, value) {
+            state.name2 = value;
+        },
+        //增加计数器
+        add(state) {
+            state.count++
+
+        }
+    },
 
 });
